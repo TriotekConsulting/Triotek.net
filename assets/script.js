7f136fbfacdd456be9fd113452cc89b016b15bef
Anders <andershbredesen@gmail.com>
2025-01-09 12:49:23 +0100

    AOS fix, email fix

--- assets/script.js
+++ assets/script.js
@@ -9,9 +9,55 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  AOS.init({
-    duration: 1200,
-    once: true,
-    offset: 100,
+  emailjs.init("R4rsFxm-pE_aCZCHs");
+
+  const form = document.getElementById("consultation-form");
+  const thankYouMessage = document.getElementById("thank-you-message");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    emailjs
+      .send("service_3d3a7h4", "template_2aap4hi", {
+        name: name,
+        email: email,
+        message: message,
+      })
+      .then(
+        function (response) {
+          console.log("E-post sendt!", response.status, response.text);
+
+          form.classList.add("uk-hidden");
+          thankYouMessage.classList.remove("uk-hidden");
+        },
+        function (error) {
+          console.error("Feil ved sending av e-post:", error);
+          alert("Det oppsto en feil. Vennligst prøv igjen senere.");
+        }
+      );
+
+    console.log({
+      name: name,
+      email: email,
+      message: message,
+    });
   });
 });
+
+window.addEventListener("load", function () {
+  AOS.refresh();
+});
+
+window.onload = function () {
+  AOS.init({
+    duration: 600,
+    once: true,
+    offset: 200,
+    throttleDelay: 200,
+    debounceDelay: 50,
+  });
+};
